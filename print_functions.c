8a997b94e341b59e966d290af23c5e7bab991f5c
Mapaseko <133228470+Mapaseko@users.noreply.github.com>
2023-07-26 09:31:28 +0200

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -4,7 +4,7 @@
 
 /**
  * print_char - Prints a char
- * By Iva and Mapaseko
+ * By Iva and Mapaseko winning team
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
