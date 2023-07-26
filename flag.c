94d62d8aa6de1610855c31d2829fc783a7c3225c
Mapaseko <133228470+Mapaseko@users.noreply.github.com>
2023-07-26 09:27:41 +0200

    Update flag.c

--- flag.c
+++ flag.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * By Iva and Mapaseko
+ * By Iva and Mapaseko winning team
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
