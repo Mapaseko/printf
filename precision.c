0b49c38470b8e88d1f5fef2314f286c0344ed686
Mapaseko <133228470+Mapaseko@users.noreply.github.com>
2023-07-26 09:31:01 +0200

    Update precision.c

--- precision.c
+++ precision.c
@@ -2,7 +2,7 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * By Iva and Mapaseko
+ * By Iva and Mapaseko winning
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
