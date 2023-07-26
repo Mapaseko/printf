576bd53eee7adc44611cf9e32545d6c1d44ed2fe
Mapaseko <133228470+Mapaseko@users.noreply.github.com>
2023-07-26 09:28:24 +0200

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -3,7 +3,7 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
- * By Iva and Mapaseko
+ * By Iva and Mapaseko winning team
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
